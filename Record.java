8657454036fabb06a85b231295028abeb234307e
Jack Williams <jw15326@it025957.users.bris.ac.uk>
2016-02-25 13:01:54 +0000

    sorted layout

---
+++ Record.java
@@ -0,0 +1,19 @@
+public class Record {
+	private String name;
+    private String kind;
+
+    public Record(String name, String kind)
+    {
+        this.name = name;
+        this.kind = kind
+
+    }
+
+    public void getRecord()
+
+    public void setRecord()
+
+    public void removeRecord()
+
+    public int countFields()
+}
