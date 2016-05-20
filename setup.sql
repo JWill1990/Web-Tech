f883635f85beb08a6d01169b8fc8a75ed776118c
lw15234 <lw15234@my.bristol.ac.uk>
2016-05-20 13:15:59 +0100

    users listed

--- setup.sql
+++ setup.sql
@@ -2,14 +2,12 @@ DROP TABLE IF EXISTS Follows;
 DROP TABLE IF EXISTS Upd8;
 DROP TABLE IF EXISTS Person;
 
-
-
 CREATE TABLE Person(
     id INTEGER PRIMARY KEY,
     uname TEXT NOT NULL UNIQUE,
     pword TEXT NOT NULL,
     name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
+    email TEXT NOT NULL
 );
 
 CREATE TABLE Upd8(
@@ -24,3 +22,9 @@ CREATE TABLE Follows(
     follower INTEGER REFERENCES Person(id),
     followee INTEGER REFERENCES Person(id)
 );
+
+INSERT INTO Person VALUES (null, 'liamw', 'pass1', 'Liam', 'liam@email.com');
+INSERT INTO Person VALUES (null, 'jackw', 'pass2', 'Jack', 'jack@email.com');
+INSERT INTO Person VALUES (null, 'ianh', 'pass3', 'Ian', 'ian@email.com');
+INSERT INTO Person VALUES (null, 'supercool123', 'pass4', 'Super Cool', 'sc123@email.com');
+INSERT INTO Person VALUES (null, 'compsci', 'pass5', 'Computer Science', 'compsci@email.com');
